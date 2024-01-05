a2cd0e099715042ad1a9b8f0cafb63b2b3386135
GloryKO <koladeg2022@gmail.com>
2024-01-05 12:47:06 +0100

    added user profile and management

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
 
     path('register/',views.CreateUserView.as_view(),name='register'),
+    path('token/',views.CreateUserTokenView.as_view(),name='user_token'),
+    path('/profile/',views.ManageUserView.as_view(),name='login'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
-from . serializers import UserSerializer
+from rest_framework.generics import CreateAPIView,RetrieveUpdateAPIView
+from . serializers import UserSerializer,AuthTokenSerializer
+from rest_framework import permissions,authentication
+from rest_framework.authtoken.views import ObtainAuthToken
 
 class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
+
+class CreateUserTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+
+class ManageUserView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes =[permissions.IsAuthenticated]
+    authentication_classes =[authentication.TokenAuthentication]
+
