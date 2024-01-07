c33dd8e9c118725f26274e8f27c0fc4b1a247cba
GloryKO <koladeg2022@gmail.com>
2024-01-07 23:09:03 +0100

    added test for authenticated user test

--- posts/tests.py
+++ posts/tests.py
@@ -18,6 +18,7 @@ class PublicPostApiTests(TestCase):
         self.user = get_user_model().objects.create_user(name='testname',email='testuser@example.com', password='testpass')
 
     def test_retrieve_posts(self):
+        """test post retrieval """
         Post.objects.create(title='Test Post 1', content='Content 1', author=self.user)
         Post.objects.create(title='Test Post 2', content='Content 2', author=self.user)
 
@@ -30,8 +31,24 @@ class PublicPostApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
     
     def test_unauthenticated_post_returns_error(self):
+        """ test unathorized posts """
         payload={'title':'test post','content':'test content'}
         res=self.client.post(POSTS_URL,payload)
         self.assertEqual(res.status_code,status.HTTP_403_FORBIDDEN)
+
+class PrivatePostApitest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(name='testname',email='test@example.com',password='testpass')
+        self.client.force_authenticate(self.user)
+
+    def test_authenticated_user_post(self):
+        """Test authenticated user post successful """
+        payload={'title':'test title','content':'test content'}
+        res = self.client.post(POSTS_URL,payload)
+        self.assertEqual(res.status_code,status.HTTP_201_CREATED)
+        post = Post.objects.get(id=res.data['id'])
+        self.assertEqual(post.title,res.data['title'])
+        self.assertEqual(post.content,res.data['content'])
 
 
