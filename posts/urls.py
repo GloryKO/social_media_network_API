5afcc7e6e5634a7110b1c070e0a0466210223f74
GloryKO <koladeg2022@gmail.com>
2024-01-10 12:37:47 +0100

    added view for user-specific posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PostListView,PostDetailView
+from .views import PostListView,PostDetailView,userPostsListView
 
 urlpatterns = [
     path('posts/',PostListView.as_view(),name='post-list'),
     path('posts/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
-    
+    path('users_posts/<int:user_id>/posts/',userPostsListView.as_view(),name='user-posts-list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics,permissions
 from . serializers import PostSerializer
 from . models import Post
 from . permissions import IsAuthorOrReadOnly
-
+from django.contrib.auth import get_user_model
 
 class PostListView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -19,3 +19,11 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsAuthorOrReadOnly,)
 
+class userPostsListView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes =[permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        user=get_user_model().objects.get(id=user_id)
+        return Post.objects.filter(author=user)
